d5f7d0e59be8999bf08e99988cf6abd2ea9c7157
Phillip <phillip.besse@gmail.com>
2026-06-29 00:05:35 -0400

    add Vessel class

---
+++ backend/app/schemas.py
@@ -0,0 +1,12 @@
+from typing import List
+from pydantic import BaseModel, Field
+
+class Vessel(BaseModel):
+    name: str = Field(..., example="Example Vessel")
+    vessel_type: str = Field(..., example="powerboat")
+    length_ft: float = Field(..., gt=0, example=32)
+    fuel_capacity_gallons: float = Field(..., gt=0, example=200)
+    cruise_speed_knotsL: float = Field(..., gt=0, example=22)
+    fuel_burn_gph: float = Field(..., ge=0, example=18)
+    reserve_requirement_percent: float = Field(..., ge=0, le=100, example=20)
+    
